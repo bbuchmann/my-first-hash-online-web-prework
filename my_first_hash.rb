b95019839b05f8199c9bd3b4e3b921bc29dd44aa
Ben Buchmann <ben_buchy92@hotmail.com>
2019-05-11 18:30:37 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,4 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+def Shipping_manifest = {"Whale bone corsets" => 5, "Vase" => 2, "oil paintings" => 3} 
 end
 
 
